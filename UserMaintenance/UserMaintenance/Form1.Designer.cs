86c4081090ff817806af7bc6bce97bb68b2916f1
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2021-10-01 11:02:57 +0200

    Refactor Name handling

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -31,9 +31,7 @@ namespace UserMaintenance
         {
             this.listUsers = new System.Windows.Forms.ListBox();
             this.txtLastName = new System.Windows.Forms.TextBox();
-            this.txtFirstName = new System.Windows.Forms.TextBox();
             this.lblLastName = new System.Windows.Forms.Label();
-            this.lblFirstName = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -52,13 +50,6 @@ namespace UserMaintenance
             this.txtLastName.Size = new System.Drawing.Size(142, 20);
             this.txtLastName.TabIndex = 1;
             // 
-            // txtFirstName
-            // 
-            this.txtFirstName.Location = new System.Drawing.Point(295, 43);
-            this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(142, 20);
-            this.txtFirstName.TabIndex = 2;
-            // 
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
@@ -68,15 +59,6 @@ namespace UserMaintenance
             this.lblLastName.TabIndex = 3;
             this.lblLastName.Text = "label1";
             // 
-            // lblFirstName
-            // 
-            this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(221, 46);
-            this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(35, 13);
-            this.lblFirstName.TabIndex = 4;
-            this.lblFirstName.Text = "label2";
-            // 
             // btnAdd
             // 
             this.btnAdd.Location = new System.Drawing.Point(224, 78);
@@ -85,6 +67,7 @@ namespace UserMaintenance
             this.btnAdd.TabIndex = 5;
             this.btnAdd.Text = "button1";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // Form1
             // 
@@ -92,9 +75,7 @@ namespace UserMaintenance
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 289);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.lblFirstName);
             this.Controls.Add(this.lblLastName);
-            this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.listUsers);
             this.Name = "Form1";
@@ -108,9 +89,7 @@ namespace UserMaintenance
 
         private System.Windows.Forms.ListBox listUsers;
         private System.Windows.Forms.TextBox txtLastName;
-        private System.Windows.Forms.TextBox txtFirstName;
         private System.Windows.Forms.Label lblLastName;
-        private System.Windows.Forms.Label lblFirstName;
         private System.Windows.Forms.Button btnAdd;
     }
 }
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -19,21 +19,22 @@ namespace UserMaintenance
         {
             InitializeComponent();
 
-            lblLastName.Text = Resource1.LastName; 
-            lblFirstName.Text = Resource1.FirstName;
+            lblLastName.Text = Resource1.FullName;            
             btnAdd.Text = Resource1.Add;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
             listUsers.DisplayMember = "FullName";
 
-            var u = new User()
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            User u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+                FullName = txtLastName.Text
             };
             users.Add(u);
-             
         }
     }
 }
